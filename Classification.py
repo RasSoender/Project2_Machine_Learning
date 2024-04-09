7ce32b7c701063df8a94be9bc6620eaf1c11ab04
soender2 <s214925@dtu.dk>
2024-04-09 14:01:56 +0200

    Dummy and dummy

--- Classification.py
+++ Classification.py
@@ -4,56 +4,34 @@ from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
 from sklearn import model_selection
 from dtuimldmtools import rlr_validate, train_neural_net
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.dummy import DummyClassifier
 
-def baseline_error(baseline, y_test):
-    error = 0
+def baseline_error(X_train, y_train, X_test, y_test):
     
-    # Determine the majority class in y_test by taking the sum of y_test and comparing it to the number of elements in y_test
-    class_predict = 1 if np.sum(y_test) > (y_test.shape[0] / 2) else 0
-    
-    for i in y_test:
-        if i != class_predict:
-            error += 1
-    return error / y_test.shape[0]
+    dummy_clf = DummyClassifier(strategy="most_frequent")
+    dummy_clf.fit(X_train, y_train)
+    # Make predictions
+    y_est = dummy_clf.predict(X_test)
+
+    error = np.sum(y_est!=y_test)/y_test.shape[0]
+
+    return error
 
 
-def train_ANN(hidden, y, train_index, test_index):
-    y = y.reshape(-1, 1)
+def train_KNN(neighbors, X_train, y_train, X_test, y_test):
     errors = []
-    for h in hidden: 
-        model = lambda: torch.nn.Sequential(
-            torch.nn.Linear(M, h),  # M features to n_hidden_units
-            torch.nn.ReLU(),  # 1st transfer function,
-            torch.nn.Linear(h, 1),  # n_hidden_units to 1 output neuron
-            torch.nn.Sigmoid()  # Sigmoid activation for binary classification
-        )
-        loss_fn = torch.nn.BCELoss()  # Binary Cross-Entropy Loss
-        # Extract training and test set for current CV fold, convert to tensors
-        X_train = torch.Tensor(X[train_index, :])
-        y_train = torch.Tensor(y[train_index])
-        X_test = torch.Tensor(X[test_index, :])
-        y_test = torch.Tensor(y[test_index])
-        max_iter = 10000
-        n_replicates = 1
-        # Train the net on training data
-        net, _, _ = train_neural_net(
-            model,
-            loss_fn,
-            X=X_train,
-            y=y_train,
-            n_replicates=n_replicates,
-            max_iter=max_iter,
-        )
-        # Determine estimated class labels for test set
-        y_test_est = (net(X_test) > 0.5).float()  # Threshold at 0.5 for binary classification
+    for n in neighbors: 
+        knclassifier = KNeighborsClassifier(n_neighbors=n)
+        knclassifier.fit(X_train, y_train)
+        y_est = knclassifier.predict(X_test)
 
-        # Determine accuracy as the evaluation metric
-        accuracy = (y_test_est == y_test).float().mean().data.numpy()
-        errors.append(1 - accuracy)  # Error is 1 - accuracy for binary classification
+        errors.append(np.sum(y_est!=y_test)/y_test.shape[0])
+        
     min_error = min(errors)
-    h = errors.index(min_error)
+    n = neighbors[errors.index(min_error)]
 
-    return h, min_error
+    return n, min_error
 
 
 def train_regression(lambda_interval, X_train, y_train, X_test, y_test):
@@ -114,6 +92,8 @@ lambdas = np.power(10.0, range(-5, 9))
 # Values of lambda
 hidden = list(range(1, 10))
 
+neighbors = list(range(1,20))
+
 # Define outer CV
 outer_cv = KFold(n_splits=K1, shuffle=True)
 inner_cv = KFold(n_splits=K2, shuffle=True)
@@ -126,9 +106,9 @@ for train_outer_index, test_outer_index in outer_cv.split(X, y):
     X_test_outer = X[test_outer_index]
     y_test_outer = y[test_outer_index]
 
-    best_error_ANN = float('inf')
+    best_error_KNN = float('inf')
     best_error_Reg = float('inf')
-    best_hidden_units = None
+    best_neighbors = None
     best_lambda = None
 
     k_inner = 1
@@ -139,7 +119,7 @@ for train_outer_index, test_outer_index in outer_cv.split(X, y):
         y_test_inner = y_train_outer[test_inner_index]
 
         # # Train ANN
-        # h, E_ann = train_ANN(hidden, y_train_outer, train_inner_index, test_inner_index)
+        n, E_knn = train_KNN(neighbors, X_train_inner, y_train_inner, X_test_inner, y_test_inner)
 
         # # Train logistic regression
         lambda_log_reg, E_log_reg = train_regression(lambdas, X_train_inner, y_train_inner, X_test_inner, y_test_inner)
@@ -148,22 +128,22 @@ for train_outer_index, test_outer_index in outer_cv.split(X, y):
             best_error_Reg = E_log_reg
             best_lambda = lambda_log_reg
 
-        # if E_ann < best_error_ANN:
-        #     best_error_ANN = E_ann
-        #     best_hidden_units = h
+        if E_knn < best_error_KNN:
+            best_error_KNN = E_knn
+            best_neighbors = n
 
         k_inner += 1
 
     # Train models on the optimal hyperparameters
-    # _, outer_fold_ann_error = train_ANN([best_hidden_units], y, train_outer_index, test_outer_index)
+    _, outer_fold_knn_error = train_KNN([best_neighbors], X_train_outer, y_train_outer, X_test_outer, y_test_outer)
     _, outer_fold_reg_error = train_regression([best_lambda], X_train_outer, y_train_outer, X_test_outer, y_test_outer)
 
     # Compute baseline error
-    baseline_error_outer = baseline_error(y_train_outer, y_test_outer)
+    baseline_error_outer = baseline_error(X_train_outer, y_train_outer, X_test_outer, y_test_outer)
 
     # Print results for the outer fold
     print(f"Outer Fold Nr. {k_outer}")
-    # print(f"ANN: h = {best_hidden_units}, error = {outer_fold_ann_error}")
+    print(f"KNN: n = {best_neighbors}, error = {outer_fold_knn_error}")
     print(f"Regression: lambda = {best_lambda}, error = {outer_fold_reg_error}")
     print(f"Baseline: {baseline_error_outer}")
 
